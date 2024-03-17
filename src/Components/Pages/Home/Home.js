af11ea1e802548e1e750ba08d1b3bc6678f48780
Rafael Badaro <rafabadaro.15@gmail.com>
2024-03-17 17:29:47 -0300

    making background image darker

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -4,7 +4,9 @@ function Home() {
   return (
     <>
       <div className="home-content custom-content-padding margin-top-auto view-height-100">
-        <h1>home</h1>
+        <div className=''>
+          <h1>home</h1>
+        </div>
       </div>
     </>
   );
