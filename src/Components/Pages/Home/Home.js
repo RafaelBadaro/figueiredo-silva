4d49cc5b51394d2398db26f1af9e23065dac6b2c
Rafael Badaro <rafabadaro.15@gmail.com>
2024-03-17 17:15:32 -0300

    fixing backgorund image

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 function Home() {
   return (
     <>
-      <div className="home-content custom-content-padding margin-top-auto">
+      <div className="home-content custom-content-padding margin-top-auto view-height-100">
         <h1>home</h1>
       </div>
     </>
